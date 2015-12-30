a44b06ec10da07de135a70aa9e00ce2548729e89
Bogdan Popa <popa.bogdanp@gmail.com>
2015-12-30 11:32:16 +0200

    add pythonrc

---
+++ pythonrc.py
@@ -0,0 +1,64 @@
+import os
+import pprint
+import readline
+import rlcompleter  # noqa
+import sys
+
+from code import InteractiveConsole
+from tempfile import mkstemp
+
+readline.parse_and_bind("tab: complete")
+
+EDITOR = "emacsclient"
+EDITOR_COMMAND = r"\e"
+
+
+class REPL(InteractiveConsole):
+    def __init__(self, *args, **kwargs):
+        self.last_buffer = []
+        InteractiveConsole.__init__(self, *args, **kwargs)
+
+    def runsource(self, source, *args, **kwargs):
+        self.last_buffer = [source.encode("utf-8")]
+        return InteractiveConsole.runsource(self, source, *args, **kwargs)
+
+    def raw_input(self, *args, **kwargs):
+        line = InteractiveConsole.raw_input(self, *args, **kwargs)
+        if line == EDITOR_COMMAND:
+            fd, filename = mkstemp(".py")
+
+            os.write(fd, b"\n".join(self.last_buffer))
+            os.close(fd)
+            os.system("%s %s" % (EDITOR, filename))
+
+            with open(filename) as f:
+                lines = f.readlines()
+                if not lines:
+                    return ""
+
+                for i, line in enumerate(lines):
+                    if i == len(lines) - 1:
+                        break
+
+                    self.push(line)
+
+            os.unlink(filename)
+
+        return line
+
+
+def displayhook(value):
+    if value is not None:
+        try:
+            import __builtin__
+
+            __builtin__._ = value
+        except ImportError:
+            __builtins__._ = value
+
+        pprint.pprint(value)
+
+sys.displayhook = displayhook
+
+c = REPL(locals=locals())
+sys.exit(c.interact(banner=""))
