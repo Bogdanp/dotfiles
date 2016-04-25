556fe3f32a2d5677c407a1d3e84d14970dfe4181
Bogdan Popa <popa.bogdanp@gmail.com>
2016-04-25 09:04:38 +0300

    add php and composer

--- fish/config.fish
+++ fish/config.fish
@@ -22,6 +22,7 @@ set -xg PATH $PATH /usr/local/google-cloud-sdk/bin
 set -xg PATH $PATH $HOME/bin
 set -xg PATH $PATH $HOME/.cargo/bin
 set -xg PATH $PATH $GOROOT/bin $GOPATH/bin $GOGAE
+set -xg PATH /usr/local/php5/bin $HOME/.composer/vendor/bin $PATH
 set -xg MANPATH /usr/share/man /usr/local/share/man /opt/X11/share/man /usr/local/MacGPG2/share/man /Library/TeX/Distributions/.DefaultTeX/Contents/Man
 set -xg RUST_SRC_PATH $HOME/sandbox/rust/src
 
