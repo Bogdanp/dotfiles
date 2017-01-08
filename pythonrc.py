60cca7a5221608e38b5888a1b17419b3ed0a1560
Bogdan Popa <popa.bogdanp@gmail.com>
2017-01-08 10:25:57 +0200

    colorize python prompts

--- pythonrc.py
+++ pythonrc.py
@@ -58,6 +58,9 @@ def displayhook(value):
 
         pprint.pprint(value)
 
+
+sys.ps1 = "\033[0;34m>>> \033[0m"
+sys.ps2 = "\033[1;34m... \033[0m"
 sys.displayhook = displayhook
 
 c = REPL(locals=locals())
