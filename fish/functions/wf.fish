9e1df8643c29f6e5de5965c801d8a9e7731a0dc3
Bogdan Popa <popa.bogdanp@gmail.com>
2017-02-19 15:52:39 +0200

    add wf function

---
+++ fish/functions/wf.fish
@@ -0,0 +1,6 @@
+function wf
+  find ~/{sandbox,work} -type d -mindepth 1 -maxdepth 1 | xargs basename | fzf-tmux --query="$argv[1]" --select-1 --exit-0 | read -l project
+  if [ $project ]
+    workon $project
+  end
+end
