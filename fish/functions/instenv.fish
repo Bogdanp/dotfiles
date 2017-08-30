e75b47bea10e3aed5e24a4098d971942990e8664
Bogdan Popa <popa.bogdanp@gmail.com>
2017-08-30 11:36:01 +0300

    test for requirements/dev.txt in instenv

--- fish/functions/instenv.fish
+++ fish/functions/instenv.fish
@@ -4,6 +4,8 @@ function instenv
     set reqs requirements-dev.txt
   else if test -e requirements_dev.txt
     set reqs requirements_dev.txt
+  else if test -e requirements/dev.txt
+    set reqs requirements/dev.txt
   end
 
   pip install -U pip flake8 jedi rope importmagic autopep8 gnureadline -r $reqs
