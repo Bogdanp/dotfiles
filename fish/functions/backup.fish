094cdb1626c36f8a51857185de23b45b5ed1a9be
Bogdan Popa <bogdan@defn.io>
2019-05-06 20:35:38 +0300

    drop dropbox

--- fish/functions/backup.fish
+++ fish/functions/backup.fish
@@ -1,3 +1,3 @@
 function backup
-    tarsnap -c -f "mbp-"(date +%Y-%m-%d_%H-%M-%S) ~/.ssh ~/Dropbox/Preferences ~/Mail
+    tarsnap -c -f "mbp-"(date +%Y-%m-%d_%H-%M-%S) ~/.ssh ~/Documents ~/Mail
 end
