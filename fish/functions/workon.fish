c5090cd6e1a99315ec7f2662852d8331a712b23a
Bogdan Popa <bogdan@defn.io>
2019-07-27 10:17:50 +0300

    fish: load nvm config on projects that have one

--- fish/functions/workon.fish
+++ fish/functions/workon.fish
@@ -1,22 +1,26 @@
 function workon
-  if test (count $argv) -eq 1;
+  if test (count $argv) -eq 1
     set PROJECT $argv[1]
 
-    if test -d "$HOME/sandbox/$PROJECT";
-      cd "$HOME/sandbox/$PROJECT";
+    if test -d "$HOME/sandbox/$PROJECT"
+      cd "$HOME/sandbox/$PROJECT"
 
-    else if test -d "$HOME/work/$PROJECT";
-      cd "$HOME/Work/$PROJECT";
+    else if test -d "$HOME/work/$PROJECT"
+      cd "$HOME/Work/$PROJECT"
 
-    else;
-      echo "'$PROJECT' not found";
-      return 1;
+    else
+      echo "'$PROJECT' not found"
+      return 1
 
-    end;
+    end
 
-    if test -d "$HOME/.virtualenvs/$PROJECT";
+    if test -d "$HOME/.virtualenvs/$PROJECT"
       vf activate "$PROJECT"
-    end;
+    end
+
+    if test -f ".nvmrc"
+      nvm use
+    end
 
     tmux rename-window "$PROJECT"
   else;
