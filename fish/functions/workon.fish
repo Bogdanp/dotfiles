5fb4bf5763f7357fd89f438faaeec695116cc591
Bogdan Popa <bogdan@defn.io>
2019-12-04 11:47:45 +0200

    fish: reindent workon

--- fish/functions/workon.fish
+++ fish/functions/workon.fish
@@ -1,30 +1,33 @@
 function workon
-  if test (count $argv) -eq 1
-    set PROJECT $argv[1]
+    if test (count $argv) -eq 1
+        set PROJECT $argv[1]
 
-    if test -d "$HOME/sandbox/$PROJECT"
-      cd "$HOME/sandbox/$PROJECT"
+        if test -d "$HOME/sandbox/$PROJECT"
+            cd "$HOME/sandbox/$PROJECT"
 
-    else if test -d "$HOME/work/$PROJECT"
-      cd "$HOME/Work/$PROJECT"
+        else if test -d "$HOME/work/$PROJECT"
+            cd "$HOME/work/$PROJECT"
 
-    else
-      echo "'$PROJECT' not found"
-      return 1
+        else if test -d (find "$HOME/sandbox/golang/src" -type d -name "$PROJECT")
+            cd (find "$HOME/sandbox/golang/src" -type d -name "$PROJECT")
 
+        else
+            echo "'$PROJECT' not found"
+            return 1
+
+        end
+
+        if test -d "$HOME/.virtualenvs/$PROJECT"
+            vf activate "$PROJECT"
+        end
+
+        if test -f ".nvmrc"
+            nvm use
+        end
+
+        tmux rename-window "$PROJECT"
+    else;
+        echo "usage: workon PROJECT"
+        return 1
     end
-
-    if test -d "$HOME/.virtualenvs/$PROJECT"
-      vf activate "$PROJECT"
-    end
-
-    if test -f ".nvmrc"
-      nvm use
-    end
-
-    tmux rename-window "$PROJECT"
-  else;
-    echo "usage: workon PROJECT"
-    return 1
-  end
 end
